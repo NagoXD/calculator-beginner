1aa4115033186b46dce09852bddccfb3a60b918d
Petro Mazur <114314376+NagoXD@users.noreply.github.com>
2023-03-02 17:58:04 +0100

    first commit

---
+++ intro.js
@@ -0,0 +1,3 @@
+var user = { name: "hittesk", age: 10 };
+console.log("Hello");
+console.log(user.email);
---
+++ src/script.ts
@@ -0,0 +1,30 @@
+"use strict";
+
+
+let Inputfield = document.getElementById("InputField") as HTMLInputElement;
+
+function handleClick(value: string){
+    const operators = ["+","-","*","/"];
+    const sliceChart = Inputfield.value.slice(-1);
+    if(value === `C`){
+        Inputfield.value = ""
+    } else if (operators.includes(sliceChart) && operators.includes(value)) {}
+     else if (value === "." && Inputfield.value.includes(".")) {}
+    else {
+        Inputfield.value += value
+    } 
+}
+// Using eval can be dangerous and lead to security vulnerabilities
+ /* function handleCalculate(){
+   let result = eval(Inputfield.value)
+   Inputfield.value = result
+}
+
+*/
+
+function  handleCalculate(){
+    const inputValue = Inputfield.value
+    const validInput = inputValue.replace(/[^-()\d/*+.]/g, "")
+    const result = Function(`'use strict'; return (${validInput})`)();
+    Inputfield.value = result;
+}
